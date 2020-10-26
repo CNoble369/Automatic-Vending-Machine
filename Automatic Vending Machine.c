edb58e2a8d3b0f210c0dd992fb81c79d9688d7f7
CNoble369 <68541682+CNoble369@users.noreply.github.com>
2020-10-26 17:03:23 +0530

    Add files via upload
    The Menu List is here,
    Coffee
    1. Epresso Coffee
    2. Cappuccino Coffee
    3. Black Coffee
    
    Meals
    1. Roast Chicken
    2. White Bean Soup
    3. Sandwich
    4. Fried Chicken
    5. Vegetable Roast
    6. Noodles Recipe
    
    Ice cream
    1. Kulfi
    2. Strawberry
    3. Cones
    4. Vennila
    
    Beverages
    1. Soft Drink
    2. Energy Drink
    3. Soda
    4. Lemon Juice
    5. Orange Juice
    6. Banana Juice
    7. Cold Coffee
    
    Example 1:-
    Input
    C
    1
    Welcome to AVM!
    Enjoy your Expresso Coffee.
    
    Example 2:-
    Input
    C
    4
    Invalid Input
    
    Example 3:-
    Input
    M
    3
    Enjoy your Sandwich.

---
+++ Automatic Vending Machine.c
@@ -0,0 +1,229 @@
+//Automatic Vending Machine
+#include <stdio.h>
+
+
+int main()
+
+{
+    
+  char main_menu;
+    
+  char main_items[] = {'C', 'M', 'I', 'B'};
+    
+  int sub_menu;
+    
+  int i;
+
+    
+    
+  printf("Input\n");
+    
+  scanf("%s %d", &main_menu, &sub_menu);
+  
+  
+    
+  if(main_menu == main_items[0] && sub_menu == 1)
+    
+  {
+        
+    printf("Welcome to AVM!\n");
+        
+    printf("Enjoy your Expresso Coffee.\n");
+    
+  }
+    
+  else if(main_menu == main_items[0] && sub_menu == 2)
+    
+  {
+        
+    printf("Welcome to AVM!\n");
+        
+    printf("Enjoy your Cappuccino Coffee.\n");
+    
+  }
+    
+  else if(main_menu == main_items[0] && sub_menu == 3)
+    
+  {
+        
+    printf("Welcome to AVM!\n");
+
+    printf("Enjoy your Black Coffee.\n");
+
+  }
+    
+  else if(main_menu == main_items[1] && sub_menu == 1)
+
+  {
+        
+    printf("Welcome to AVM!\n");
+        
+    printf("Enjoy your Roast Chicken.\n");
+    
+  }
+    
+  else if(main_menu == main_items[1] && sub_menu == 2)
+    
+  {
+        
+    printf("Welcome to AVM!\n");
+
+    printf("Enjoy your White Bean Soup.\n");
+   
+  }
+
+  else if(main_menu == main_items[1] && sub_menu == 3)
+    
+  {
+        
+    printf("Welcome to AVM!\n");
+        
+    printf("Enjoy your Sandwiche.\n");
+    
+  }
+    
+  else if(main_menu == main_items[1] && sub_menu == 4)
+    
+  {
+        
+    printf("Welcome to AVM!\n");
+        
+    printf("Enjoy your Fried Chicken.\n");
+    
+  }
+    
+  else if(main_menu == main_items[1] && sub_menu == 5)
+    
+  {
+        
+    printf("Welcome to AVM!\n");
+        
+    printf("Enjoy your Vegetable Roast.\n");
+    
+  }
+    
+  else if(main_menu == main_items[1] && sub_menu == 6)
+    
+  {
+        
+    printf("Welcome to AVM!\n");
+        
+    printf("Enjoy your Noodles Recipe.\n");
+    
+  }
+    
+  else if(main_menu == main_items[2] && sub_menu == 1)
+    
+  {
+        
+    printf("Welcome to AVM!\n");
+        
+    printf("Enjoy your Kulfi.\n");
+    
+  }
+    
+  else if(main_menu == main_items[2] && sub_menu == 2)
+   
+  {
+        
+    printf("Welcome to AVM!\n");
+        
+    printf("Enjoy your Strawberry.\n");
+    
+  }
+    
+  else if(main_menu == main_items[2] && sub_menu == 3)
+    
+  {
+        
+    printf("Welcome to AVM!\n");
+        
+    printf("Enjoy your Cones.\n");
+    
+  }
+    
+  else if(main_menu == main_items[2] && sub_menu == 4)
+    
+  {
+        
+    printf("Welcome to AVM!\n");
+        
+    printf("Enjoy your Vennila.\n");
+    
+  }
+    
+  else if(main_menu == main_items[3] && sub_menu == 1)
+    
+  {
+        
+    printf("Welcome to AVM!\n");
+        
+    printf("Enjoy your Soft Drink.\n");
+    
+  }
+    
+  else if(main_menu == main_items[3] && sub_menu == 2)
+    
+  {
+        
+    printf("Welcome to AVM!\n");
+        
+    printf("Enjoy your Energy Drink.\n");
+    
+  }
+    
+  else if(main_menu == main_items[3] && sub_menu == 3)
+    
+  {
+        
+    printf("Welcome to AVM!\n");
+        
+    printf("Enjoy your Soda.\n");
+    
+  }
+    
+  else if(main_menu == main_items[3] && sub_menu == 4)
+    
+  {
+        
+    printf("Welcome to AVM!\n");
+        
+    printf("Enjoy your Lemon Juice.\n");
+    
+  }
+    
+  else if(main_menu == main_items[3] && sub_menu == 5)
+    
+  {
+        
+    printf("Welcome to AVM!\n");
+        
+    printf("Enjoy your Orange Juice.\n");
+    
+  }
+    
+  else if(main_menu == main_items[3] && sub_menu == 6)
+    
+  {
+        
+    printf("Welcome to AVM!\n");
+        
+    printf("Enjoy your Banana Juice.\n");
+    
+  }
+    
+  else if(main_menu == main_items[3] && sub_menu == 7)
+    
+  {
+        
+    printf("Welcome to AVM!\n");
+        
+    printf("Enjoy your Cold Coffee.\n");
+    
+  }
+    
+  else
+        
+    printf("Invalid Input\n");
+
+}    
